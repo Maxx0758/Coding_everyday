0980777491cae861a8a6070569471300e4852ac3
Maxx0758 <32611523+Maxx0758@users.noreply.github.com>
2019-04-02 21:18:02 +0200

    Made the mouse able to register the three doors

--- The_Doors - Max.py
+++ The_Doors - Max.py
@@ -14,8 +14,37 @@ runde = 1
 while(running):
     try:
         window = pygame.display.set_mode((width,height))
+        pygame.time.delay(100)
+        pygame.display.set_caption("The doors")
         mousePos = pygame.mouse.get_pos()
         print(mousePos)
+        print(runde)
+        #Looking for alle the events, thats happening
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                if 100 <= mousePos[0] <= 300 and 200 <= mousePos[1] <= 600:
+                    if runde == 1:
+                        runde = 2
+                    elif runde == 2:
+                        runde = 3
+                    elif runde == 3:
+                        running = False
+                elif 650 <= mousePos[0] <= 850 and 200 <= mousePos[1] <= 600:
+                    if runde == 1:
+                        runde = 2
+                    elif runde == 2:
+                        runde = 3
+                    elif runde == 3:
+                        running = False
+                elif 1200 <= mousePos[0] <= 1400 and 200 <= mousePos[1] <= 600:
+                    if runde == 1:
+                        runde = 2
+                    elif runde == 2:
+                        runde = 3
+                    elif runde == 3:
+                        running = False
         if runde == 1:
             window.fill((255, 255, 255))
             pygame.draw.rect(window, brown, pygame.Rect((door1[0], door1[1]), (doorWidth, doorHeight)))
