29ca76680bf967e813195ecaea6fd3926fe9a73a
alex123a <45672183+alex123a@users.noreply.github.com>
2019-04-03 21:32:06 +0200

    Update Mystery door game - Blex - Kopi.py

--- Mystery door game - Blex - Kopi.py
+++ Mystery door game - Blex - Kopi.py
@@ -1,29 +1,47 @@
 from random import randint
 import math
-import pygame
+import pygame, sys
 from pygame.locals import *
 
 pygame.init()
 
+window = pygame.display.set_mode([800,600])
+
 running = True
 
-runde = 1
+runde = True
 
 White = (255, 255, 255)
 
 Blue = (0, 0, 255)
 
+pygame.font.init()
+
+clock = pygame.time.Clock()
+font = pygame.font.SysFont("comicsansms", 30)
+font2 = pygame.font.SysFont("comicsansms", 15)
+text = font.render("What is the most effective transport", True, (255, 255, 255))
+text2 = font2.render("Public transport", True, (255, 255, 255))
+text3 = font2.render("Bike", True, (255, 255, 255))
+text4 = font2.render("Car", True, (255, 255, 255))
 
 while(running):
     try:
         mousePos = pygame.mouse.get_pos()
         print(mousePos)
+        pygame.time.delay(100)
 
-        if runde == 1:
-            
-            window = pygame.display.set_mode([800,600])
+        if runde == True:
 
             window.fill((0,0,0))
+
+            window.blit(text, (400 - text.get_width() // 2, 30 - text.get_height() // 2))
+            window.blit(text2, (150 - text2.get_width() // 2, 75 - text2.get_height() // 2))
+            window.blit(text3, (400 - text3.get_width() // 2, 75 - text3.get_height() // 2))
+            window.blit(text4, (650 - text4.get_width() // 2, 75 - text4.get_height() // 2))
+    
+            pygame.display.flip()
+            clock.tick(60)
 
             door1 = pygame.draw.rect(window, White, Rect((50, 100),(200, 400)))
             print(door1)
@@ -42,21 +60,20 @@ while(running):
                 elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                     if door1.collidepoint(mousePos):
                         print("Forkert noob")
-                        andenRunde = 2
+                        running = False
                         break
                     elif door2.collidepoint(mousePos):
                         print("Forkert noob")
-                        andenRunde = 2
+                        running = False
                         break
                     elif door3.collidepoint(mousePos):
                         print("Når når du er pro")
-                        andenRunde = 2
+                        andenRunde = True
+                        runde = False
                         break
 
         
-        if andenRunde == 2:
-
-            window = pygame.display.set_mode([800,600])
+        if andenRunde == True:
 
             window.fill((0,0,0))
 
@@ -76,19 +93,20 @@ while(running):
                     running = False
                 elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                     if door1.collidepoint(mousePos):
-                        print("Forkert noob")
-                        tredjeRunde == 3
+                        print("Når når du er pro")
+                        tredjeRunde = True
+                        andenRunde = False
                         break
                     elif door2.collidepoint(mousePos):
                         print("Forkert noob")
-                        tredjeRunde == 3
+                        running = False
                         break
                     elif door3.collidepoint(mousePos):
-                        print("Når når du er pro")
-                        tredjeRunde == 3
+                        print("Forkert noob")
+                        running = False
                         break
 
-        if tredjeRunde == 3:
+        if tredjeRunde == True:
             break
 
     except Exception as e:
@@ -96,5 +114,6 @@ while(running):
         print("Unknown error")
 
 print("\n Tak for spillet")
+pygame.QUIT()
 
         
