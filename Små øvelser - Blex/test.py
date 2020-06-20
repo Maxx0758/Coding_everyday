eec8ff82dd92f85cb02ff95168d942727b288a33
Alex <avnyborg2@gmail.com>
2020-06-21 00:26:35 +0200

    Ændringer til guess the number

---
+++ Små øvelser - Blex/test.py
@@ -0,0 +1,26 @@
+# import matplotlib.pylab as plt
+
+# plt.plot([1, 2, 3, 4], [3, 4, 9, 16])
+# plt.xlabel('x')
+# plt.ylabel('y')
+# plt.show()
+
+# import pygame
+
+# # Intialize the pygame
+# pygame.init()
+
+# # Create the screen with width and height
+# width = 800
+# height = 600
+# screen = pygame.display.set_mode((width, height))
+
+# running = True
+
+# while running:
+#     print("yeet")
+
+# liste = ['Hej', 'Med', 'Dig']
+
+# for i in enumerate(liste):
+#     print(f'Index er {i[0]} og value er {i[1]}')
